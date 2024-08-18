306deba54623fd91b59e0d39cc570aa24bed63c3
Ragnilde <ragnildeity@gmail.com>
2024-08-18 23:07:27 +0700

    New Release 1.0

--- scripts/flags.js
+++ scripts/flags.js
@@ -1,6 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
     fetch('../json/flags.json')
-        .then(response => response.json())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
         .then(data => {
             const tbody = document.querySelector('#flags-table tbody');
 
